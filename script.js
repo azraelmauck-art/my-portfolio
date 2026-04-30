66acca807543f06c2eed6a94d0de22b173184ea0
azraelmauck-art <azraelmauck@gmail.com>
2026-04-30 17:35:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+  
+document.getElementById("glow").onmouseover = function() {mouseOver()};
+document.getElementById("glow").onmouseout = function() {mouseOut()};
 
+function mouseOver() {
+  document.getElementById("glow").style.color = "red";
+}
+
+function mouseOut() {
+  document.getElementById("glow").style.color = "black";
+}
+    console.log("Page loaded!")
+});
